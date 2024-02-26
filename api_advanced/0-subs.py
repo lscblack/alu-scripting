cfd4b22c0d36416446b37d182ea0fd50e846315a
lscblack <l.christian@alustudent.com>
2024-02-26 18:24:30 +0200

    done all

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -18,3 +18,13 @@ def number_of_subscribers(subreddit):
         return subscriber_number
     else:
         return 0
+
+import sys
+
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
