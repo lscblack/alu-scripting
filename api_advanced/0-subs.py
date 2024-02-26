d1f59123dd7c5c4ccfe0d64bfcb6b1493891d22a
lscblack <l.christian@alustudent.com>
2024-02-26 18:32:18 +0200

    done all

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,17 @@
-#!/usr/bin/python3
-"""Script that fetch number of subscriber for a given subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return number of subscribers is @subreddit is valid subreddit.
-    if not return 0."""
-
+    """Return the number of subscribers for the given subreddit."""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    subreddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
-        json_data = response.json()
-        subscriber_number = json_data.get('data').get(
-            'children')[0].get('data').get('subreddit_subscribers')
-        return subscriber_number
+        try:
+            json_data = response.json()
+            subscriber_count = json_data['data']['subscribers']
+            return subscriber_count
+        except KeyError:
+            return 0
     else:
         return 0
