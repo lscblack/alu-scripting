b01b58f066b1749b1aebb3c1b52b4814a85d7a2c
lscblack <l.christian@alustudent.com>
2024-02-26 18:46:22 +0200

    sub

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -11,12 +11,4 @@ def number_of_subscribers(subreddit):
     subreddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code == 200:
-        try:
-            json_data = response.json()
-            subscriber_count = json_data['data']['subscribers']
-            return "subscriber_count"
-        except KeyError:
-            return "as"
-    else:
-        return "fd"
+    return 'OK'
