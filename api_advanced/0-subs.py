e7f2e80f2587a91db83bdff6820f303cf9cd1e9c
lscblack <l.christian@alustudent.com>
2024-03-07 15:30:17 +0200

    as

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """ Return the number of subscribers """
 
-
 import json
 import requests
 import sys
 
-
 def number_of_subscribers(subreddit):
-    """ Return  subscribers"""
+    """ Return the number of subscribers """
     if len(sys.argv) < 2:
         return 0
     else:
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
+        # Provide your Reddit account credentials
+        auth = requests.auth.HTTPBasicAuth('lscblack', '7hfXGzp37@wQS8!')
+        result = requests.get(url, headers=headers, auth=auth, allow_redirects=False)
         if result.status_code != 200:
             return 0
         body = json.loads(result.text)
         return body["data"]["subscribers"]
+
+# Test the function
+if __name__ == "__main__":
+    subreddit = input("Enter the subreddit name: ")
+    subscribers = number_of_subscribers(subreddit)
+    print("Subscribers in r/{}: {}".format(subreddit, subscribers))
