081099803e83a442ade13d02e22dc1ba85c6bef5
lscblack <l.christian@alustudent.com>
2024-03-07 15:32:00 +0200

    asd

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-""" Return the number of subscribers """
-
-import json
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
-import sys
+
 
 def number_of_subscribers(subreddit):
-    """ Return the number of subscribers """
-    if len(sys.argv) < 2:
-        return 0
-    else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        # Provide your Reddit account credentials
-        auth = requests.auth.HTTPBasicAuth('lscblack', '7hfXGzp37@wQS8!')
-        result = requests.get(url, headers=headers, auth=auth, allow_redirects=False)
-        if result.status_code != 200:
-            return 0
-        body = json.loads(result.text)
-        return body["data"]["subscribers"]
+    """Return the number of subscribers for the given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            return data.get('subscribers', 0)
+    return 0
